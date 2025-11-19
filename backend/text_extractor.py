5e84c33b6076a9c9e4888f92105007a090db108e
Andy Li <andyli@Andys-Laptop.local>
2025-11-19 17:23:43 -0500

    Doc Extraction, Chunking, and Embedding

---
+++ backend/text_extractor.py
@@ -0,0 +1,18 @@
+import pdfplumber
+
+def extract_pdf_text(path):
+    text = ""
+    with pdfplumber.open(path) as pdf:
+        for page in pdf.pages:
+            page_text = page.extract_text()
+            if page_text:
+                text += page_text + "\n"
+    return text
+
+#---TESTING---#
+
+PDF_PATH = "/Users/andyli/Downloads/indexccl.pdf"
+
+all_text = extract_pdf_text(PDF_PATH)
+print(f"Extracted {len(all_text)} characters from PDF.")
+print(all_text[:500])  
