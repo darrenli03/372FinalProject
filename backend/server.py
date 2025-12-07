50cfcd4865696b49267fb4552c9bf438b4330700
darrenli03 <71399017+darrenli03@users.noreply.github.com>
2025-12-06 23:08:34 -0500

    idk why google cloud build failed, tryna change port number

--- backend/server.py
+++ backend/server.py
@@ -38,5 +38,5 @@ async def query_single(
 if __name__ == "__main__":
     import uvicorn
     
-    port = int(os.environ.get("PORT", os.getenv("API_PORT", "8000")))
+    port = int(os.getenv("PORT", "8080"))
     uvicorn.run(app, host="0.0.0.0", port=port, log_level="info")
